555238c14715a03822a3e844c8474c6d2db8f940
HYD <hanyd@pku.edu.cn>
2021-08-03 14:36:11 +0800

    ImageNet-LT

--- DeepSense/keras_impl.py
+++ DeepSense/keras_impl.py
@@ -20,7 +20,7 @@ bash ./swarm-learning/bin/run-sl        \
     -genJoinToken
 """
 
-# In[1]:
+
 
 
 import numpy as np
@@ -31,6 +31,8 @@ import tensorflow.keras as keras
 from os.path import join as ospj
 import os
 from datetime import datetime
+
+from tensorflow.python.ops.gen_nn_ops import SeluGrad
 
 SWARM_LEARNING = os.getenv('SLNUM') is not None
 
@@ -47,42 +49,20 @@ if gpus:
             tf.config.experimental.set_virtual_device_configuration(gpu, [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=4096)])
         logical_gpus = tf.config.experimental.list_logical_devices('GPU')
         print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        pass
     except RuntimeError as e:
         # Memory growth must be set before GPUs have been initialized
         print(e)
 
-# In[2]:
 
 DATA_DIR=os.getenv('DATA_DIR', './data')
 DATA_ROOT=ospj(DATA_DIR, 'ruifeng')
-BATCH_SIZE=32
-
-
-# In[3]:
+BATCH_SIZE=64
 
 
 data = None
-with open(ospj(DATA_DIR, 'ruifeng_by_date.json'), 'r') as fp:
+with open('ruifeng_by_date.json', 'r') as fp:
     data = json.load(fp)
-print(data.keys())
-
-
-# In[4]:
-
-
-print(data['img_size'])
-print(data['label_index'])
-
-
-# In[5]:
-
-
-img=plt.imread(ospj(DATA_ROOT, data['train']['pos'][0][0]))
-print(img.shape)
-plt.imshow(img)
-
-
-# In[6]:
 
 
 def get_dataset(pos_list, neg_list, data_root='data', training=True, batch_size=BATCH_SIZE):
@@ -91,7 +71,7 @@ def get_dataset(pos_list, neg_list, data_root='data', training=True, batch_size=
         image = tf.image.decode_jpeg(image, channels=3)
         image = tf.image.resize(image, data['img_size'][:2])
         image = tf.cast(image, tf.float32)
-        image = keras.applications.efficientnet.preprocess_input(image)
+        image = keras.applications.densenet.preprocess_input(image)
         # image = tf.image.per_image_standardization(image)
         return image
 
@@ -123,16 +103,12 @@ def get_dataset(pos_list, neg_list, data_root='data', training=True, batch_size=
     return ds
 
 
-# In[7]:
-
-
 train_dataset = get_dataset(data_root=DATA_ROOT, pos_list=data['train']['pos'], neg_list=data['train']['neg'], training=True)
 val_dataset = get_dataset(data_root=DATA_ROOT, pos_list=data['val']['pos'], neg_list=data['val']['neg'], training=False)
 steps_per_epoch=np.math.ceil((len(data['train']['pos'])+len(data['train']['neg']))/BATCH_SIZE)
 steps_per_epoch
 
 
-# In[8]:
 #strategy = tf.distribute.MirroredStrategy()
 #print('Number of devices: {}'.format(strategy.num_replicas_in_sync))
 
@@ -148,18 +124,33 @@ def DenseNet49(include_top=True,
                 input_shape=None,
                 pooling=None,
                 classes=1000):
-    """Instantiates the Densenet169 architecture."""
+    """Instantiates the Densenet49 architecture."""
     return DenseNet([4, 4, 8, 6], include_top, weights, input_tensor, input_shape, pooling, classes)
 
-# model = DenseNet49(include_top=True, weights=None, input_shape=data['img_size'], classes=2)
-model = keras.applications.EfficientNetB0(include_top=True, weights=None, classes=2, input_shape=data['img_size'])
+model = DenseNet49(include_top=True, weights=None, input_shape=data['img_size'], classes=2)
+#model = keras.models.load_model('saved_models/DenseNet49-DeepSense-baseline-20210725-194831-baseline/DenseNet49-DeepSense-baseline-50.hdf5')
+# model = keras.applications.EfficientNetB0(include_top=True, weights=None, classes=2, input_shape=data['img_size'])
+# model.compile(
+#     optimizer='adam',
+#     loss='categorical_crossentropy'
+# )
+# model.evaluate(val_dataset)
+# model.save('enetv2')
+# exit(0)
 
-model_name = 'EfficientNetB0-DeepSense'
+# model = keras.models.load_model('enetv2')
+model_name = 'DenseNet49-Sense++'
+
 if SWARM_LEARNING:
     model_name += '-SL-{0}'.format(os.getenv('SLNUM'))
+else:
+    model_name += '-baseline'
+    model.save(model_name)
+
+sgd = keras.optimizers.SGD(learning_rate=keras.optimizers.schedules.CosineDecay(0.01, 500))
 
 model.compile(
-    optimizer='adam',
+    optimizer=sgd,
     loss='categorical_crossentropy',
     metrics=[
         'accuracy',
@@ -167,27 +158,32 @@ model.compile(
         *[keras.metrics.Precision(name='{0}-Precision'.format(x), class_id=x) for x in range(2)]]
 )
 
-
-# In[10]:
+current_name = model_name
+swSyncInterval = steps_per_epoch * 5
+min_peers = 3
+if SWARM_LEARNING:
+    current_name += '-SL{0}-interval{1}-peers{2}'.format(os.getenv('SLNUM'), swSyncInterval, min_peers)
+else:
+    current_name += '-baseline'
+current_name += datetime.now().strftime("-%Y%m%d%H%M%S")
 
 
 try:
     os.mkdir('./saved_models')
-    os.mkdir('./saved_models/' + datetime.now().strftime("%Y%m%d-%H%M%S"))
+    os.mkdir('./saved_models/' + current_name)
 except:
     pass
 
 callbacks=[
-    keras.callbacks.ModelCheckpoint('saved_models/' + datetime.now().strftime("%Y%m%d-%H%M%S") + '/enetv2b0-{epoch:02d}.hdf5'),
+    keras.callbacks.ModelCheckpoint('saved_models/' + current_name + '/' + model_name + '-{epoch:03d}'),
     keras.callbacks.TensorBoard(
-        log_dir="logs/profile/" + datetime.now().strftime("%Y%m%d-%H%M%S"),
-        profile_batch=3
+        log_dir="logs/profile/" + current_name,
+        profile_batch=10
     )
 ]
 
 if SWARM_LEARNING:
-    swSyncInterval = steps_per_epoch
-    min_peers = 2
+    print("SWARM-LERANING!")
     from swarm import SwarmCallback
     swarmCallback = SwarmCallback(
         sync_interval=swSyncInterval,
@@ -199,7 +195,7 @@ if SWARM_LEARNING:
     )
     callbacks.append(swarmCallback)
 
-max_epochs = 10
+max_epochs = 200
 
 model.fit(
     train_dataset,
